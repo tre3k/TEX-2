e663eade2000ba6a80fd99fa77d16e0f5ec95b2a
Kirill <pshcyrill@mail.ru>
2019-07-16 15:27:34 +0300

    start write tdc class

--- servers/Detector/test/main.cpp
+++ servers/Detector/test/main.cpp
@@ -1,34 +1,11 @@
 #include <iostream>
 
-#include "plx9030c.h"
+#include "tdc.h"
 
 int main(int argc, char **argv){
   char byte;
 
-  PLX9030::plx9030 *plx = new PLX9030::plx9030("/dev/plxdev0");
+  TDC *tdc = new TDC();
 
-  plx->write8(0x00, PLX9030::CS0,0);
-  
-  for(int i=0;i<32;i++){
-    byte = plx->read8(PLX9030::CS0, i);
-    std::cout << std::dec << "CS0+" << i
-	      << " : 0x" << std::hex << (byte&0xff) << "\t|";
-
-    byte = plx->read8(PLX9030::CS1, i);
-    std::cout << std::dec << "CS1+" << i
-	      << " : 0x" << std::hex << (byte&0xff) << "\t|";
-  
-    byte = plx->read8(PLX9030::CS2, i);
-    std::cout << std::dec << "CS2+" << i
-	      << " : 0x" << std::hex << (byte&0xff) << "\t|";
-
-    byte = plx->read8(PLX9030::CS3, i);
-    std::cout << std::dec << "CS3+" << i
-	      << " : 0x" << std::hex << (byte&0xff) << "\n";
-  
-  
-  }
-
-  delete plx;
   return 0;
 }
